26d629edaeb711ad9cded0e126eea3302a5387c9
Dragos <dufi_x@yahoo.com>
2023-12-13 09:40:17 +0200

    git igonre

--- Src/main.c
+++ Src/main.c
@@ -10,7 +10,7 @@
 #include "main.h"
 #include "stdint.h"
 #include "init.h"
-#include "Generic.h"
+#include "generic.h"
 
 /* Private variables ---------------------------------------------------------*/
 
